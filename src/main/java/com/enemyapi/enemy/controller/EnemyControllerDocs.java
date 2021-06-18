a1e368c9a538098bb6dc8aefbbc3f241c2ef55e1
José D Afonseca <josepfj1@gmail.com>
2021-06-18 10:15:02 -0300

    creation documentation of the API

---
+++ src/main/java/com/enemyapi/enemy/controller/EnemyControllerDocs.java
@@ -0,0 +1,42 @@
+package com.enemyapi.enemy.controller;
+
+import com.enemyapi.enemy.dto.request.EnemyDTO;
+import com.enemyapi.enemy.exception.EnemyNotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Api("Manages enemy registration")
+public interface EnemyControllerDocs {
+
+    @ApiOperation(value = "Enemy creation operation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Success enemy creation"),
+            @ApiResponse(code = 400, message = "Missing required fields or wrong field range value.")
+    })
+    EnemyDTO createEnemy(EnemyDTO enemyDTO) throws EnemyAlreadyRegisteredException;
+
+    @ApiOperation(value = "Returns enemy found by a given ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success enemy found in the system"),
+            @ApiResponse(code = 404, message = "Enemy with given ID not found.")
+    })
+    EnemyDTO findById(@PathVariable Long id) throws EnemyNotFoundException;
+
+    @ApiOperation(value = "Returns a list of all enemies registered in the system")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "List of all enemies registered in the system"),
+    })
+    List<EnemyDTO> listAll();
+
+    @ApiOperation(value = "Delete a enemy by a given valid ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Success enemy deleted in the system"),
+            @ApiResponse(code = 404, message = "Enemy with given id not found.")
+    })
+    void deleteById(@PathVariable Long id) throws EnemyNotFoundException;
+}
